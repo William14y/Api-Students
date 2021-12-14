5fec5b7868f4d8efdafcfeacab9ba2438de3fc56
William Cavalcante Ulisses <williamcavalcante06@gmail.com>
2021-12-14 14:26:43 -0300

    Commitando test

---
+++ src/test/java/br/com/listaDeAlunos/listagemalunos/ListagemalunosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.listaDeAlunos.listagemalunos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListagemalunosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
