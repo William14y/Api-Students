355cf53ae9d4766d453bf99f515217599bf81b09
William Cavalcante Ulisses <williamcavalcante06@gmail.com>
2021-12-14 14:20:53 -0300

    Commitando Application

---
+++ src/main/java/br/com/listaDeAlunos/listagemalunos/ListagemalunosApplication.java
@@ -0,0 +1,13 @@
+package br.com.listaDeAlunos.listagemalunos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListagemalunosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListagemalunosApplication.class, args);
+	}
+
+}
